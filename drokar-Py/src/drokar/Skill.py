026bd1687ce91302a1948e99067110b0eecdaefc
JoelKirkby <joelkirkby@gmail.com>
2023-08-02 08:33:48 -0600

    run_task to be a class method of Skill Class, which imports a dictionary of tasks on init

--- drokar-Py/src/drokar/Skill.py
+++ drokar-Py/src/drokar/Skill.py
@@ -1,4 +1,35 @@
+from drokar.Item import Item
+import time
+
+
+mining_tasks = {'Copper Ore':
+                    {'lvl_requirement':1,
+                     'XP_gain':10,
+                     'item_yield':[Item('Copper Ore')],
+                     'action_time':1.5
+                     },
+                 'Tin Ore': {
+                     'lvl_requirement':1,
+                     'XP_gain':10,
+                     'item_yield':[Item('Tin Ore')],
+                     'action_time':1.5
+                 }     
+                } 
+
+metallurgy_tasks = {'Bronze Bar':
+                    {'lvl_requirement':1,
+                     'XP_gain':20,
+                     'item_yield':1,
+                     'requires':['Copper Ore', 'Tin Ore'],
+                     'action_time':1.5
+                     } 
+                }     
 class Skill:
+    def __init__(self,name,tasks:dict,total_xp=0):
+        self.total_xp=total_xp
+        self.name=name
+        self.tasks=tasks
+
     def run_task(self,task,inventory,RunEvent):
         task_info=self.tasks[task]
         
