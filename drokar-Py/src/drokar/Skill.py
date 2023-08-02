0ffcf3d21bf5ca4ee87899a00cbf5bb26e7f504d
JoelKirkby <joelkirkby@gmail.com>
2023-08-02 08:32:43 -0600

    Add basic task run function to loop while thread event is True and add copper to player inventory (no XP)

--- drokar-Py/src/drokar/Skill.py
+++ drokar-Py/src/drokar/Skill.py
@@ -1,3 +1,17 @@
 class Skill:
-    def __init__(self):
+    def run_task(self,task,inventory,RunEvent):
+        task_info=self.tasks[task]
+        
+        while RunEvent.is_set():
+            print(f'\nPerforming {self.name} task: {task}')
+            task_time=task_info['action_time']
+            time.sleep(task_time)
+            items = task_info['item_yield']
+            for item in items:
+                existing_item=next((i for i in inventory if i.name == item.name), False)
+                if existing_item:
+                    existing_item.quantity+=1
+                else:
+                    inventory.append(item)
+                print(f'gained 1 {item.name}, you now have {item.quantity} in total') 
         None
