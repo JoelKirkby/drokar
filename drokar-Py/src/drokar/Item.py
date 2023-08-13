cd95575c5f22729506273cbf9a449a9b00437a68
JoelKirkby <joelkirkby@gmail.com>
2023-08-12 18:17:11 -0600

    Add base Item class

--- drokar-Py/src/drokar/Item.py
+++ drokar-Py/src/drokar/Item.py
@@ -1,3 +1,23 @@
+'''Item properties
+in order
+0: Sell value - int
+1: Description - str
+2: Can equip - bool
+3: Requirements - dict {skill (str): level (int)} for each skill
+4: Combinations - dict {'item' (str) :product (Item)}
+'''
+
+item_list={'Copper Ore':[5,'A red ore, can be refined using Metallurgy',False,{}],
+           'Tin Ore': [5, 'A faint silver ore, can be refined using Metallurgy',False,{}],
+           'Silver Ore': [35, 'A shimmering silver ore, can be refined using Metallurgy',False,{}]}
+
+
 class Item:
-    def __init__(self):
+    def __init__(self,item_name):
+        item_properties=item_list[item_name]
+        self.name=item_name
+        self.sell_value, self.description, self.equippable,self.requirements=item_properties
+        self.quantity=1
+
+    def combine(self):
         None
