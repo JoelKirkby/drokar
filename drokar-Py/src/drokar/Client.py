ba647f9f90f0a3b3cc661a8225a0f7d8303d090c
JoelKirkby <joelkirkby@gmail.com>
2023-11-14 08:46:33 -0700

    feat( Client ): Set up new front end client file with flask instance

--- drokar-Py/src/drokar/Client.py
+++ drokar-Py/src/drokar/Client.py
@@ -1,4 +1,24 @@
+import os
 import threading
+import os
+import pickle
+import sys
+import threading
+
+from Entity import Player
+from Skill import SKILL_LIST, Skill
+
+from flask import Flask, render_template, request
+
+#Game data
+EQUIP_SLOTS = ["gloves", "boots", "helm", "armor", "trinket", "idol", "off_hand", "main_hand", "cloak"]
+CURRENCIES = ["Gold"]
+prosp = Skill("Prospecting")
+
+# Flask instance
+TEMPLATE_DIR = os.path.abspath("templates")
+app = Flask(__name__, template_folder=TEMPLATE_DIR)
+app.config["UPLOAD_FOLDER"] = os.path.join(os.getcwd(), f"src{os.sep}drokar{os.sep}static{os.sep}images")
 class Client:
     def __init__(self):
         None
