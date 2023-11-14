f61ec5620bd3587a89ba39bcc1f290c73d7b156d
JoelKirkby <joelkirkby@gmail.com>
2023-11-14 08:47:06 -0700

    feat( front-end ): Add basic page navigation, comment out future features

--- drokar-Py/src/drokar/Client.py
+++ drokar-Py/src/drokar/Client.py
@@ -19,6 +19,34 @@ prosp = Skill("Prospecting")
 TEMPLATE_DIR = os.path.abspath("templates")
 app = Flask(__name__, template_folder=TEMPLATE_DIR)
 app.config["UPLOAD_FOLDER"] = os.path.join(os.getcwd(), f"src{os.sep}drokar{os.sep}static{os.sep}images")
+
+
+@app.route("/")
+def index():  # noqa: D103
+    return render_template("index.html")
+
+
+@app.route("/Prospecting")
+def prospecting():
+    return render_template("skillpage_base.html", skill=prosp.name, skill_data=prosp.tasks, level=prosp.level)
+
+
+# @app.route("/Metallurgy")
+# def metallurgy():
+#     if request.method == "GET":
+#         return render_template("metallurgy.html")
+
+
+# @app.route("/Inventory")
+# def inventory():
+#     if request.method == "GET":
+#         return render_template("bank.html")
+
+
+# @app.route("/Store")
+# def store():
+#     if request.method == "GET":
+#         return render_template("store.html")
 class Client:
     def __init__(self):
         None
