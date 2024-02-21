e5e42e2b05754682e2595e7dbbee929596d524f9
JoelKirkby <joelkirkby@gmail.com>
2024-02-21 08:43:59 -0700

    Add item data objects

---
+++ drokar-js/src/helpers/ItemData.js
@@ -0,0 +1,17 @@
+import pickaxeImg from  './images/pickaxe.svg'
+import metallurgyImg from './images/metallurgy-icon.svg'
+
+
+export const ItemData = {
+    "Copper Ore":{
+        'sellValue': 5,
+        'description': "A red ore, can be refined using Metallurgy",
+        'image': pickaxeImg,
+        },
+    "Tin Ore":
+        {
+        'sellValue': 5,
+        'description': "A faint silver ore, can be refined using Metallurgy",
+        'image': metallurgyImg,
+        }
+}
