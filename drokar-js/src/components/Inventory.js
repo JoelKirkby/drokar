0b3d409166274f16e320c0f791b384cae8ab2628
JoelKirkby <joelkirkby@gmail.com>
2024-02-21 08:46:14 -0700

    Add inventory grid generation

--- drokar-js/src/components/Inventory.js
+++ drokar-js/src/components/Inventory.js
@@ -1,9 +1,34 @@
 import '../App.css'
+import InventoryItem from './InventoryItem';
+
+const DUMMY_INVENTORY = [
+    {
+        name: "Copper Ore",
+        quantity: 2
+    },
+    {
+        name: "Tin Ore",
+        quantity: 2
+    }, 
+    {
+        name: "Copper Ore",
+        quantity: 10
+    } 
+]
+
 
 function Inventory() {
     return (
       <div className="Inventory">
         Inventory
+        <div className="itemZone">
+            <div className="inventoryGrid">
+                {DUMMY_INVENTORY.map(item =>
+                    <InventoryItem item={item.name} quantity={item.quantity}/>
+                )}
+            </div>
+            <div className="equippedWindow">EquippedWindow</div>
+        </div>
       </div>
     );
   }
