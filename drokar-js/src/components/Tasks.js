eb26cb033bbfed3e426c86fe380d1e7528999299
JoelKirkby <joelkirkby@gmail.com>
2024-03-03 20:38:24 -0700

    Add function to run task and either yield or consume items based on TaskData

--- drokar-js/src/components/Tasks.js
+++ drokar-js/src/components/Tasks.js
@@ -13,3 +13,27 @@ const tasks = {
 
 
 const skillLevel = 1
+
+const runTask = (task, skill, playerData, setPlayerData) => {
+  var load_task = tasks[skill].find((obj) => obj.name == task.name)
+  let data = {...playerData};
+  
+  let i = 0;
+  for (const [costItem, costAmount] of Object.entries(load_task.consumes)) {
+     if (costItem in data.inventory && data.inventory[costItem].quantity >= costAmount) {
+      data.inventory[costItem].quantity -= costAmount
+     } else {
+      return
+     }
+    i++
+  }
+
+  for (const [yieldItem, yieldAmount] of Object.entries(load_task.yields)) {
+    yieldItem in data.inventory 
+      ? data.inventory[yieldItem].quantity += yieldAmount
+      : data.inventory[yieldItem] = {quantity: yieldAmount}
+    i++
+  }
+
+  setPlayerData(data)
+};
