b682ce12b22b9a55af7073b7d496d5b5b27c1196
JoelKirkby <joelkirkby@gmail.com>
2024-03-03 20:23:01 -0700

    new main function using MUI components

--- drokar-js/src/App.js
+++ drokar-js/src/App.js
@@ -6,12 +6,36 @@ import Events from './components/Events';
 
 function App() {
   return (
-    <div className="App">
-      <Skills/>
-      <Menu/>
-      <Inventory/>
-      <Events/>
-    </div>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <PlayerDataContext.Provider value={{playerData, setPlayerData, activeTask, setActiveTask}}>
+        <Drawer variant="permanent" open={true} sx={{ position: 'relative' }}>
+          {list('left')}
+        </Drawer>
+
+        <Box
+            component="main"
+            sx={{
+              backgroundColor: (theme) =>
+                theme.palette.mode === 'light'
+                  ? theme.palette.grey[100]
+                  : theme.palette.grey[900],
+              flexGrow: 1,
+              height: '100vh',
+              overflow: 'auto',
+            }}
+          >
+          <Box className='row1'>
+            <Tasks skill={activeSkill}/>
+            <Inventory/>
+          </Box>
+          <Box className="row2">
+            <Events/>
+          </Box>
+        </Box>
+      {/* </div> */}
+    </PlayerDataContext.Provider>
+    </Box>
   );
 }
 
