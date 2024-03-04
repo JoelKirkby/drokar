b37743c9737707da4af15a4e44d3ef9551814bf5
JoelKirkby <joelkirkby@gmail.com>
2024-03-03 20:42:32 -0700

    add new Items and images

--- drokar-js/src/helpers/ItemData.js
+++ drokar-js/src/helpers/ItemData.js
@@ -1,17 +1,32 @@
 import pickaxeImg from  './images/pickaxe.svg'
-import metallurgyImg from './images/metallurgy-icon.svg'
-
+import metallurgyImg from './images/greenstone.svg'
+import barImg from './images/bar.svg'
+import stoneImg from './images/stone.svg'
 
 export const ItemData = {
     "Copper Ore":{
         'sellValue': 5,
         'description': "A red ore, can be refined using Metallurgy",
-        'image': pickaxeImg,
+        'image': stoneImg,
         },
     "Tin Ore":
         {
         'sellValue': 5,
         'description': "A faint silver ore, can be refined using Metallurgy",
         'image': metallurgyImg,
-        }
+        },
+
+    "Silver Ore":
+        {
+        'sellValue': 5,
+        'description': "A faint silver ore, can be refined using Metallurgy",
+        'image': metallurgyImg,
+        },
+    
+    'Bronze Bar':
+        {
+            sellValue: 15, 
+            description: 'A refined brown bar which can be used to make weapons, armor, and vessels',
+            image: barImg,
+        }   
 }
