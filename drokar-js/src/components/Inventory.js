ef10bce7d049a11ba7dc9cb10ba0f09e1e0fddd7
JoelKirkby <joelkirkby@gmail.com>
2024-03-03 20:25:46 -0700

    use player data object for inventory data

--- drokar-js/src/components/Inventory.js
+++ drokar-js/src/components/Inventory.js
@@ -1,36 +1,12 @@
 import '../App.css'
 import InventoryItem from './InventoryItem';
+import { PlayerDataContext } from '../helpers/Contexts';
+import { useContext } from 'react';
 
-const DUMMY_INVENTORY = [
-    {
-        name: "Copper Ore",
-        quantity: 2
-    },
-    {
-        name: "Tin Ore",
-        quantity: 2
-    }, 
-    {
-        name: "Copper Ore",
-        quantity: 10
-    } 
-]
-
-// #TO DO - Searching inventory to update items
-// const inventory = [
-//     { name: "apples", quantity: 2 },
-//     { name: "bananas", quantity: 0 },
-//     { name: "cherries", quantity: 5 },
-//   ];
-  
-//   function isCherries(fruit) {
-//     return fruit.name === "cherries";
-//   }
-  
-//   console.log(inventory.find(isCherries));
-//   // { name: 'cherries', quantity: 5 }
-  
 function Inventory() {
+    const {playerData, setPlayerData} = useContext(PlayerDataContext)
+    var inventory_items = playerData.inventory
+    
     return (
       <div className="Inventory">
         Inventory
