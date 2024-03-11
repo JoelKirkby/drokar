7df5d0c01dac75ed1b67fb397d53e2256a1e4dad
JoelKirkby <joelkirkby@gmail.com>
2024-03-10 23:49:58 -0600

    only pass item quantity not entire object

--- drokar-js/src/components/InventoryItem.js
+++ drokar-js/src/components/InventoryItem.js
@@ -1,12 +1,14 @@
 import { ItemData } from "../helpers/ItemData";
 
-function InventoryItem({itemName, attr}) {
+
+
+function InventoryItem({itemName, quantity}) {
     return (
       <div className="inventoryItem">
         <div className="inventoryImgContainer"> 
           <img src={ItemData[itemName].image} className='inventoryGridImg' alt="inventoryitem"/>
         </div>
-        <div className="inventoryItemQuantity">{attr.quantity}</div>
+        <div className="inventoryItemQuantity">{quantity}</div>
       </div>
     );
   }
