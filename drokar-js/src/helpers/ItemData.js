db5b72e8c5dca455faa1d596261ddfc1a1d02243
JoelKirkby <joelkirkby@gmail.com>
2024-03-13 14:45:33 -0600

    Add bronze items to ItemData

--- drokar-js/src/helpers/ItemData.js
+++ drokar-js/src/helpers/ItemData.js
@@ -23,10 +23,46 @@ export const ItemData = {
         'image': metallurgyImg,
         },
     
+    // Bronze items - Metallurgy
     'Bronze Bar':
         {
             sellValue: 15, 
             description: 'A refined brown bar which can be used to make weapons, armor, and vessels',
-            image: barImg,
-        }   
+            image: bar,
+        },
+    'Bronze Axe':
+        {
+            sellValue: 20,
+            description: "A crude axe fashioned from Bronze that can be used to fight.",
+            image: bronzeAxe,
+            equip: 'weapon',
+        },
+    'Bronze Helmet':
+    {
+        sellValue: 20,
+        description: "A crude helmet fashioned from Bronze that can be used to fight.",
+        image: platehelm,
+        equip: 'helmet',
+    },
+    'Bronze Buckler':
+    {
+        sellValue: 20,
+        description: "A crude shield from Bronze that can be used for protection.",
+        image: shield,
+        equip: 'offHand',
+    },
+    'Bronze Leggings':
+    {
+        sellValue: 20,
+        description: "Crude plate skirt fashioned from Bronze that can be used to fight.",
+        image: plateleg,
+        equip: 'leg',
+    },
+    'Bronze Platemail':
+    {
+        sellValue: 20,
+        description: "Crude platemail fashioned from Bronze that can be used to fight.",
+        image: platemail,
+        equip: 'body',
+    },
 }
