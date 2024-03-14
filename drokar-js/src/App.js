984d4ea69f4e42c9e4f165a9a0a49c03885e83f6
JoelKirkby <joelkirkby@gmail.com>
2024-03-13 21:57:57 -0600

    Custom styling of Material UI drawer

--- drokar-js/src/App.js
+++ drokar-js/src/App.js
@@ -3,6 +3,34 @@ import Skills from  './components/Skills';
 import Menu from  './components/Menu';
 import Inventory from './components/Inventory';
 import Events from './components/Events';
+const drawerWidth = 240;
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    '& .MuiDrawer-paper': {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      boxSizing: 'border-box',
+      ...(!open && {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+          width: theme.spacing(9),
+        },
+      }),
+    },
+  }),
+);
+
 const calculateLevels = ((playerData) => {
   var levels = {}
   for (const [skill, xp] of Object.entries(playerData.skills)) {
