554ed3a5fddc14e04326ea5c1186d230f88ae1d1
JoelKirkby <joelkirkby@gmail.com>
2024-03-18 09:09:14 -0600

    make combatStats a prop, as well as loot table and drop chance

--- drokar-js/src/helpers/MonsterData.js
+++ drokar-js/src/helpers/MonsterData.js
@@ -3,67 +3,77 @@ import goblinWizard from './images/goblinWizard.svg';
 
 export const MonsterData = {
     'Goblin':{
-        level: 2,
-        img: goblin,
-        maxHp: 50,
-        maxMana: 10,
-        furyRate: 0,
-        damage: 2, 
-        attackSpeed: 2600, //ms
-        // attacks: {"Attack":  TODO - Keep it simple for now.
-        //             {
-        //             accuracy: 0.8,
-        //             speed: 2600,
-        //             damage: 10,
-        //             type: "melee",
-        //             manacost: 0,
-        //             }
-        //         },
-        attackChances: [1],
-        armor: 0,
-        rangedArmor: 0,
-        magicArmor: 0,
+        combatStats: {
+            level: 2,
+            img: goblin,
+            maxHp: 80,
+            maxMana: 10,
+            furyRate: 0,
+            damage: 2, 
+            attackSpeed: 2600, //ms
+            attacks: [
+                        {
+                        name: "Attack",
+                        accuracy: 0.8,
+                        speed: 2600,
+                        damage: 2,
+                        type: "melee",
+                        manacost: 0,
+                        }
+            ],
+            attackChances: [1],
+            armor: 0,
+            rangedArmor: 0,
+            magicArmor: 0,
+        },
+        dropRates: [0.2],
+        drops: ["Tin Ore"],
     },
 
-    'Goblin Wizard' :{
-        level: 4,
-        img: goblinWizard,
-        maxHp: 50,
-        maxMana: 10,
-        furyRate: 1,
-        // Keeping it simple with just damage and speed, will allow for multiple attacks and types of attacks later.
-        damage: 2,
-        attackSpeed: 2600, //ms
-        // attacks: {"Attack": 
-        //             {
-        //             accuracy: 0.8,
-        //             speed: 2600,
-        //             damage: 2,
-        //             type: "melee",
-        //             manacost: 0,
-        //             },
-        //             "Earth Shard":
-        //             {
-        //             accuracy: 0.8,
-        //             speed: 5000,
-        //             damage: 8,
-        //             type: "magic",
-        //             manacost: 8,
-        //             }
-        //         },
-        attackChances: [1],
-        furyAttacks : {"WAAAAAAAGH": 
+    'Goblin Wizard' : {
+        combatStats: {
+            level: 4,
+            img: goblinWizard,
+            maxHp: 8,
+            maxMana: 10,
+            furyRate: 1,
+            // Keeping it simple with just damage and speed, will allow for multiple attacks and types of attacks later.
+            damage: 2,
+            attackSpeed: 2600, //ms
+            attacks: [
                         {
-                        accuracy: 0.7,
-                        speed: 4000,
-                        damage: 15,
-                        type: "magic",
+                        name: "Attack",
+                        accuracy: 0.8,
+                        speed: 2600,
+                        damage: 2,
+                        type: "melee",
                         manacost: 0,
                         },
+                        {
+                        name: "Earth Shard",
+                        accuracy: 0.8,
+                        speed: 5000,
+                        damage: 8,
+                        type: "magic",
+                        manacost: 8,
+                        }
+            ],
+            attackChances: [0.8, 0.2],
+            furyAttacks : {"WAAAAAAAGH": 
+                            {
+                            accuracy: 0.7,
+                            speed: 4000,
+                            damage: 15,
+                            type: "magic",
+                            manacost: 0,
+                            },
+            },
+            furyAttackChances : [1],
+            armor:0,
+            rangedArmor: 0,
+            magicArmor: 0,
         },
-        furyAttackChances : [1],
-        armor:0,
-        rangedArmor: 0,
-        magicArmor: 0,
-    },        
+        dropRates: [0.2],
+        drops: ["Bronze Axe"],        
+    }
 }
