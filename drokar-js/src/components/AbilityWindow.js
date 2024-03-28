39d19489716f3a784d5707442f872910bbf8c9d6
Chooky <joelkirkby@gmail.com>
2024-03-27 23:12:30 -0400

    Add AbilityRow components, and nest in flex container

--- drokar-js/src/components/AbilityWindow.js
+++ drokar-js/src/components/AbilityWindow.js
@@ -4,18 +4,35 @@ import { PlayerDataContext } from '../helpers/Contexts';
 import { useContext } from 'react';
 import Inventory from './Inventory';
 import ItemInfoPanel from './ItemInfoPanel';
+import { ABILITIES } from '../helpers/AbilityData';
+import { useState } from 'react';
+import AbilityRow from './AbilityRow';
+
+import './AbilityWindow.css'
 
 function AbilityWindow(props) {
     const {activeTask, setActivetask, playerData} = useContext(PlayerDataContext)
+    const [activeVocation, setActiveVocation] = useState('')
     return (
-        <div>
+        <div className="flexContainer">
             <div className="vocationList">
+              vocationList
+              {Object.keys(ABILITIES).map(function(name, i) {
+                return <div className="vocationBox" onClick={(e) => setActiveVocation(name)}>
+                    <img src={ABILITIES.name.img} alt={`${name} icon`}/>
+                    {name} - Level {playerData.skills[name]}
+                    </div>
+                })}    
             </div>
             <div className="skillSelection">
+              skillSelection
               <div className="equippedSkills">
-                Equipped skills
+                <AbilityRow data={playerData.activeVocation}/>
               </div>
-              <div className="equippedSkills">
+              <div className="availableSkills">
+                {activeVocation 
+                ? <AbilityRow data={ABILITIES[activeVocation]}/>
+                : null}
                 Available skills
               </div>
 
