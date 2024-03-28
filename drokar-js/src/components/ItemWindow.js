be8ce3036d959704298839986303bec97d70bf79
Chooky <joelkirkby@gmail.com>
2024-03-27 22:43:49 -0400

    Add basic ItemWindow component to show inventory, equipment, bonuses

---
+++ drokar-js/src/components/ItemWindow.js
@@ -0,0 +1,19 @@
+import '../App.css'
+import { PlayerDataContext } from '../helpers/Contexts';
+import { useContext } from 'react';
+import Inventory from './Inventory';
+import Equipment from './Equipment';
+import CombatDetails from './CombatDetails';
+
+function ItemWindow(props) {
+    const {playerData, setPlayerData} = useContext(PlayerDataContext)
+    return (
+        <div className="flexContainer">
+            <Inventory/>
+            <Equipment playerData={playerData} setPlayerData={setPlayerData}/>
+            <CombatDetails/>
+      </div>
+    );
+  }
+  
+  export default ItemWindow;
