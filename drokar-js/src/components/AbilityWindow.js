8c56c2b9b17da2422507e08be29530c49eefb334
JoelKirkby <joelkirkby@gmail.com>
2024-04-13 23:58:39 -0600

    Iterate through all ability vocation and render AbilityClassWindow component

--- drokar-js/src/components/AbilityWindow.js
+++ drokar-js/src/components/AbilityWindow.js
@@ -6,38 +6,23 @@ import AbilityEquippedWindow from './AbilityEquippedWindow';
 
 
 function AbilityWindow(props) {
-    const {activeTask, setActivetask, playerData, activeVocation, setActiveVocation} = useContext(PlayerDataContext)
-    console.log(`playerData.activeVocation: ${playerData.activeVocation}`)
     return (
         <div className="flexContainer">
             <div className="vocationList">
-              Acolyte abilities
-              <AbilityRow vocation="Acolyte"/>
-              Ruffian abilities
-              <AbilityRow vocation="Ruffian"/>
-              Squire abilities
-              <AbilityRow vocation="Squire"/>
+              {
+                Object.keys(ABILITIES).map((vocation) => 
+                {
+                  return <AbilityClassWindow vocation={vocation}/>
+                }
+                )
+              }
+      
             </div>
-                return <div className="vocationBox" onClick={(e) => setActiveVocation(ability.name)}>
-                    <img src={ability.img} alt={`${ability.name} icon`}/>
-                    {ability.name} - Level {playerData.skills[ability.name]}
-                    </div>
-                })}    
-            </div>
-            <div className="skillSelection">
-              skillSelection
-              <div className="equippedSkills">
-                <AbilityRow data={playerData.activeVocation}/>
-              </div>
-              <div className="availableSkills">
-                {activeVocation 
-                ? <AbilityRow data={activeVocation}/>
-                : null}
-                Available skills
-              </div>
 
-            </div>
-      </div>
+            <AbilityEquippedWindow/>
+
+
+         </div>
     );
   }
   
