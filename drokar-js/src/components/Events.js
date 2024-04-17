57c55e458bb22aee71734562c9280dcbe771e306
JoelKirkby <joelkirkby@gmail.com>
2024-04-17 00:57:39 -0600

    Add a mini text tutorial to Events component. Better task descriptions

--- drokar-js/src/components/Events.js
+++ drokar-js/src/components/Events.js
@@ -8,18 +8,19 @@ function Events(props) {
     let skill = activeTask.skill
     return (
       <div className="Events">
-        Events
-        <div>{activeTask.name}</div>
-
         {JSON.stringify(activeTask) === "{}"
-          ? null
-          : <ProgressLine key={playerData.skills[skill].toString()+activeTask.name} visualParts={[ 
+          ? <div className="testo"><p className="infoPanel">Click on Prospecting or Metallurgy to start a task or start fighting from the Combat window.
+          <br/> Mine ores with Prospecting, smelt them with Metallurgy, smith bars into weapons and armor and fight the goblins! 
+          <br/> You will unlock more tasks as you level up your skills.  </p></div>
+          :  <div>
+              {activeTask.name} - {(activeTask.duration/1000).toFixed(1)} seconds
+              <ProgressLine key={playerData.skills[skill].toString()+activeTask.name} visualParts={[ 
                 {
                   percentage:"100%",
                   color:"red",
                 }
                 ]} speed={activeTask.duration/1000}/>
-              }
+              </div>}
       </div>
     );
   }
