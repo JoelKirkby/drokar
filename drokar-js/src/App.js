25908e47bd52beb9f7b6726c9d3312e107379210
JoelKirkby <joelkirkby@gmail.com>
2024-04-27 09:55:47 -0600

    functions moved to other file, capitalize global constant

--- drokar-js/src/App.js
+++ drokar-js/src/App.js
@@ -40,73 +40,7 @@ function App() {
   const refAttackProg = useRef('') // Player previous attack progress to determine completed attack
   const refEnemyAttackProg = useRef('') // Enemy previous attack progress to determine completed attack
 
-function roll(probabilities) {
-  // Roll an outcome based on input probabilities O[n]
-  const totalProbability = probabilities.reduce((sum, probability) => sum + probability, 0);
-  const randomValue = Math.random() * totalProbability;
-  let cumulativeProbability = 0;
-
-  for (let i = 0; i < probabilities.length; i++) {
-    cumulativeProbability += probabilities[i];
-    if (randomValue < cumulativeProbability) {
-      return i;
-    }
-  }
-
-  return probabilities.length - 1;
-}
-const rollLootTable = (rates, drops) => {
-  let dropRates = [...rates]
-  let noLootProb = 1 - dropRates.reduce((a, b) => a + b, 0)
-  dropRates.push(noLootProb)
-  let lootRoll = roll(dropRates)
-
-  if (lootRoll === (dropRates.length -1)) {
-    return ['', 0]
-  }
-  else {
-    return [drops[lootRoll], 1]
-  }
-}
-  const rollAttackType = (entityData) => {
-    let attackProbs = entityData.combatStats.attackChances
-    let rolledAttack
-    // Roll for fury attack if fury is full
-    if (entityData.combatStats.currentFury >= entityData.combatStats.maxFury) {
-      
-      attackProbs = entityData.combatStats.furyAttackChances
-      rolledAttack = entityData.combatStats.furyAttacks[roll(attackProbs)]
-      console.log("FURY ATTACK")
-      console.log('rolledAttack.name = ' + rolledAttack.name)
-    }
-    else {rolledAttack = entityData.combatStats.attacks[roll(attackProbs)]
-    }
-    entityData.combatStats.selectedAttack = rolledAttack.name
-    entityData.combatStats.attackSpeed = rolledAttack.speed
-    
-    return entityData
-  }
-
-
-
-function App() {
-  // Declare active states for the app
-  const [activeSkill, setActiveSkill] = useState("Prospecting")
-  const loadPlayerData = useContext(PlayerDataContext)
-  const [playerData, setPlayerData] = useState(loadPlayerData)
-  let playerLevels = useMemo(() => calculateLevels(playerData), [playerData])
-  const [activeTask, setActiveTask] = useState({})
-  const [activeMonster, setActiveMonster] = useState({})
-  const tickRate = 40;
-  const [attackProg, setAttackProg] = useState(0)
-  const [enemyAttackProg, setEnemyAttackProg] = useState(0)
-  const [activeCombat, setActiveCombat] = useState(false)
-  const [sellQuantity, setSellQuantity] = useState(1)
-
-  const [activeAttack, setActiveAttack] = useState([])
-  const [activeEnemyAttack, setActiveEnemyAttack] = useState([])
-  const refAttackProg = useRef('')
-  const refEnemyAttackProg = useRef('')
+  // Manage combat events for each game tick
   useEffect(() => {
     let newPlayerData = {...playerData}
     let newMonsterData = {...activeMonster}
@@ -208,7 +142,7 @@ function App() {
       }, [attackProg, enemyAttackProg])
 
   const launchCombat = (activeCombat, setActiveCombat, playerData, setPlayerData, activeMonster, setActiveMonster, setAttackProg, setEnemyAttackProg, activeTask, setActiveTask) => {
-    // Calculate per tick progression based on attackSpeed and tickRate
+    // Calculate per tick progression based on attackSpeed and TICKRATE
 
     // Clear running Prospecting or Metallurgy task if it exists
     clearInterval(activeTask.taskId)
@@ -225,18 +159,18 @@ function App() {
     refEnemyAttackProg.current = 0
 
     // Calculate attack progression per tick
-    let prog = 100 / (playerData.combatStats.attackSpeed / tickRate)
-    let enemyProg = 100 / (activeMonster.combatStats.attackSpeed / tickRate)
+    let prog = 100 / (playerData.combatStats.attackSpeed / TICKRATE)
+    let enemyProg = 100 / (activeMonster.combatStats.attackSpeed / TICKRATE)
     
 
     // If combat is not active, start the combat loop and set states
     if (!activeCombat) {
       const timer = setInterval(() => {
-          prog = 100 / (playerData.combatStats.attackSpeed / tickRate)
-          enemyProg = 100 / (activeMonster.combatStats.attackSpeed / tickRate) 
+          prog = 100 / (playerData.combatStats.attackSpeed / TICKRATE)
+          enemyProg = 100 / (activeMonster.combatStats.attackSpeed / TICKRATE) 
           setAttackProg(prev => (prev + prog)% 100)
           setEnemyAttackProg(prev => (prev + enemyProg)% 100)
-          }, tickRate)
+          }, TICKRATE)
       setActiveAttack([playerData.combatStats.selectedAttack, playerData.combatStats.attackSpeed])
       setActiveEnemyAttack([activeMonster.combatStats.selectedAttack, activeMonster.combatStats.attackSpeed])
       setPlayerData(playerData)
