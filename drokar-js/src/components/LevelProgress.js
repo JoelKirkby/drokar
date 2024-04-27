ab571c584e6106d8e88e7db88a52547e17f1f4c8
JoelKirkby <joelkirkby@gmail.com>
2024-04-27 09:53:09 -0600

    new component levelProgress

---
+++ drokar-js/src/components/LevelProgress.js
@@ -0,0 +1,20 @@
+import '../App.css'
+import './AbilityWindow.css'
+import { LinearProgress } from "@mui/material"
+import { useContext } from "react"
+import { PlayerDataContext } from "../helpers/Contexts"
+
+function LevelProgress({skill, size}) {
+  const {playerLevels, activeVocation} = useContext(PlayerDataContext)
+  return (
+      <div className='vocationXp width80'>
+        <a>Level {playerLevels[skill][0]}</a>
+        <div className="vocationProgressBar">
+          <LinearProgress style={{padding: `${size}px`, borderRadius: "5px"}} variant="determinate" value={playerLevels[skill][1]}/>
+        </div>
+        <a>{playerLevels[skill][1].toFixed(1)}%</a>
+      </div>
+  )
+}
+
+export default LevelProgress
