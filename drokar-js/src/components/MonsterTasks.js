3964cae5c5fbd7c095de2892bf6c8f27fa65f204
JoelKirkby <joelkirkby@gmail.com>
2024-05-31 22:03:30 -0600

    Create MonsterTasks.js

---
+++ drokar-js/src/components/MonsterTasks.js
@@ -0,0 +1,57 @@
+import { Button } from "@mui/material";
+import { MonsterData } from "../helpers/MonsterData";
+import { useState, useContext } from "react";
+import { PlayerDataContext } from "../helpers/Contexts";
+import "./Combat.css";
+
+const setMonsterData = (selectedMonster, setActiveMonster) => {
+    // Set current health values to max health
+    let combatStats = {...MonsterData[selectedMonster].combatStats,
+                    name: selectedMonster,
+                    currentHp: MonsterData[selectedMonster].combatStats.maxHp,
+                    currentMana: MonsterData[selectedMonster].combatStats.maxMana,
+                    currentFury: 0,
+                    maxFury: 100,
+                    }
+
+    let activeMonster = {...MonsterData[selectedMonster], 
+            combatStats: combatStats,
+            name: selectedMonster
+        }
+    setActiveMonster(activeMonster)
+}
+
+const fleeCombat = (activeCombat, setActiveCombat, setActiveMonster) => {
+    if (activeCombat) {
+        setActiveCombat(false)
+        clearInterval(activeCombat)
+    }
+    setActiveMonster({})
+}
+
+function MonsterTasks() { 
+    const [selectedMonster, setSelectedMonster] = useState('')
+    const {activeMonster, setActiveMonster, activeCombat, setActiveCombat} = useContext(PlayerDataContext)
+    return (
+        <div className="combatTasks">
+            {Object.keys(MonsterData).map(function(monsterName, i) {
+                return <div className="monsterTask" onClick={(e) => setMonsterData(monsterName, setActiveMonster)}>
+                    <img src={MonsterData[monsterName].combatStats.img} alt="monster"/>
+                    {monsterName} - Level {MonsterData[monsterName].combatStats.level}
+                    </div>
+            }         
+            )}
+            {selectedMonster 
+                ? <div>
+                    Selected Monster: {selectedMonster}
+                    <div className="fightButtons">
+                    <Button variant="contained" className="paddedBtn" color="error" onClick={() => setMonsterData(selectedMonster, setActiveMonster)}> Target </Button>
+                    <Button variant="contained" color="warning" onClick={() => fleeCombat(activeCombat, setActiveCombat, setActiveMonster)}> Flee in Terror! </Button>
+                    </div>
+                </div>
+             : null}
+        </div>
+    )
+    }
+  
+  export default MonsterTasks;
