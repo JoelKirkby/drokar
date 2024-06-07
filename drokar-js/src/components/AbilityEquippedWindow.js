fea2ff65941dd0ddd563db6ab43e960e9c343b7f
JoelKirkby <joelkirkby@gmail.com>
2024-06-07 07:50:22 -0700

    Flex abilities now properly show and will show locked/unlocked status

--- drokar-js/src/components/AbilityEquippedWindow.js
+++ drokar-js/src/components/AbilityEquippedWindow.js
@@ -21,25 +21,27 @@ function AbilityEquippedWindow({playerData}) {
             <p> At vocation levels 8 and 16 you can choose an extra passive or active ability from any other vocation. </p>
             <div className="textWrapper">
                 
-              
-            <div className='flexContainer spaceAround'>
-                <div className={ability0_className}>
-                  <p className='lvlText'>Lvl 8</p>  
-                    Ability 1 placeholder
-                      {/* <Tooltip title={abilityList[1].description}>
-                          <img src={abilityList[1].img} className="abilityImg" alt={`${abilityList[1].name} icon`}/>
-                      </Tooltip> */}
+                <div className={flex0unlocked}>
+                    <a className='lvlText'>Level 8</a>   
+                    <div className={"ability passive"}>
+                      <Tooltip title={flexAbility0.description}>
+                          {flexAbility0.img ? <img src={flexAbility0.img} className='ability passive' alt={`${flexAbility0.name} icon`}/> : <a>Equip Ability</a>}
+                      </Tooltip>
+                    </div>
                 </div>
-                <div className={ability1_className}>
-                  <p className='lvlText'>Lv 16</p>
-                    Ability 2 placeholder
-                    {/* <Tooltip title={abilityList[2].description}>
-                          <img src={abilityList[2].img} className="abilityImg" alt={`${abilityList[2].name} icon`}/>
-                      </Tooltip> */}
-                  <p>Mana Drain</p>
+
+                <div className={flex1unlocked}>
+                    <a className='lvlText'>Level 16</a>   
+                    <div className="ability passive">
+                      <Tooltip title={flexAbility1.description}>
+                        {flexAbility1.img ? <img src={flexAbility1.img} className='ability passive' alt={`${flexAbility1.name} icon`}/> : <a>Equip Ability</a>}
+                        </Tooltip>
+                    </div>
                 </div>
-            
             </div>
+            </div>
+            Flex Abilities
+              </div>
             Flex Abilities
         </div>
           </div>
