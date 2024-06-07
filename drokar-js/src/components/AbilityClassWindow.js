eba57e6452cfedcb5091b67f5874a15253f97167
JoelKirkby <joelkirkby@gmail.com>
2024-06-07 00:06:04 -0600

    Add vocation switching function which updates activeVocation and playerData states

--- drokar-js/src/components/AbilityClassWindow.js
+++ drokar-js/src/components/AbilityClassWindow.js
@@ -21,23 +21,83 @@ const descriptions = {
 
 
 const switchVocation = (vocation, playerData, setPlayerData, playerLevels, setActiveVocation) => {
+  console.log("Switching vocation")
+  let newPlayerData = {...playerData}
+  let oldVocation = playerData.activeVocation
+  let playerLevel = playerLevels[vocation][0]
+
+  // Remove old spells
+  if (oldVocation) {
+    let oldVocationLevel = playerLevels[oldVocation][0]
+    for (let unlock of ABILITIES[oldVocation]) {
+
+      if (unlock.levelRequirement <= oldVocationLevel) {
+        
+        // Remove old spell
+        if (unlock.abilityType === 'spell') {
+          console.log(`Removed ${unlock.name} to player's spellbook`)
+          newPlayerData.combatStats.spells = newPlayerData.combatStats.spells.filter(spell => spell.name !== unlock.name);
+        }
+
+        // Update active on-hit effects
+        else if (unlock.abilityType === 'proc') {
+          console.log(`Removed ${unlock.name} from onHitEffects for player`)
+          newPlayerData.combatStats.onHitEffects = newPlayerData.combatStats.onHitEffects.filter(onHit => onHit.name !== unlock.name);
+        }
+
+        // Add passive bonuses
+        else if (unlock.abilityType === 'passive') {
+          for (const [bonus, amount] of Object.entries(unlock.passiveEffect)) {
+            newPlayerData.combatStats[bonus] -= amount
+            console.log(`removed ${amount} from ${bonus} for player. Old = ${playerData.combatStats[bonus]} ,New = ${newPlayerData.combatStats[bonus]}`)
+            }
+          }
+        }
+      }
   }
+
+  // Add new spells
+  for (let unlock of ABILITIES[vocation]) {
+
+    // If level requirement is satisfied, unlock ability
+    if (unlock.levelRequirement <= playerLevel) {
+
+      // Add new spells
+      if (unlock.abilityType === 'spell') {
+        console.log(`added ${unlock.name} to player's spellbook`)
+        newPlayerData.combatStats.spells.push(unlock)
+      }
+
+      // Update active on-hit effects
+      else if (unlock.abilityType === 'proc') {
+        console.log(`added ${unlock.applyEffect} to onHitEffects for player`)
+        newPlayerData.combatStats.onHitEffects.push(unlock)
+      }
+
+      // Add passive bonuses
+      else if (unlock.abilityType === 'passive') {
+        for (const [bonus, amount] of Object.entries(unlock.passiveEffect)) {
+          console.log(`Before applying passive =${newPlayerData.combatStats[bonus]}`)
+          if (bonus in newPlayerData.combatStats) {
+            newPlayerData.combatStats[bonus] += amount
+          }
+          else {
+            newPlayerData.combatStats[bonus] = amount
+          }
+          console.log(`After applying passive to ${bonus} =${newPlayerData.combatStats[bonus]}`)
+          }
+        }
+
+      }
+    }
   
 
-  if (statOperator === '-') {
-    playerData.combatStats[parameter] -= bonus
-  }
-  else if (statOperator === '+') {
-  playerData.combatStats[parameter] += bonus
-  }
-  else if (statOperator === '*') {
-    playerData.combatStats[parameter] *= bonus
-  }
-  else {
-    playerData.combatStats[parameter] /= bonus
-  }
 
-  return playerData
+  console.log('Updating!')
+  console.log(`newPlayerData =  ${JSON.stringify(newPlayerData)}`)
+  newPlayerData.activeVocation = vocation
+  setPlayerData(newPlayerData)
+  setActiveVocation(vocation)
 }
 function AbilityClassWindow({vocation}) {
     const {activeTask, setActivetask, playerData, playerLevels, activeVocation, setActiveVocation} = useContext(PlayerDataContext)
